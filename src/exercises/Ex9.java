6912641db1bac3acdb31ba8253e853c995cca642
otavio <otaviotemoteo@gmail.com>
2025-06-10 11:41:32 -0300

    Ex9

--- src/exercises/Ex9.java
+++ src/exercises/Ex9.java
@@ -1,10 +1,16 @@
+//Write code that uses the ‘SecureRandom’ class to generate a sequence of 10 random integers and display them.
 package exercises;
+
+import java.security.SecureRandom;
 
 public class Ex9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SecureRandom secureRandom = new SecureRandom();
+		
+		for(int i=0; i<10; i++) {
+			int numero = secureRandom.nextInt();
+			System.out.print("| "+numero+" |");
+		}
 	}
-
 }
